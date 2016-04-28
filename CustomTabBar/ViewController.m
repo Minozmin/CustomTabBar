62d9f8238a4bc299e3bd6d1d59c046f93e18da73
haoshiqi <hehuimin1992@163.com>
2016-04-28 11:30:14 +0800

    Initial Commit

---
+++ CustomTabBar/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CustomTabBar
+//
+//  Created by haoshiqi on 16/4/28.
+//  Copyright © 2016年 haoshiqi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
